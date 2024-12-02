ed72d7a5da2a92df236ca5d6bf2873236be4a2a8
Moneem Ahmed <moneem141@gmail.com>
2024-12-02 21:07:00 +0600

    create initial UI components for Home and More tabs and update project configuration: remove unnecessary .idea files, add .idea and .kotlin to .gitignore

---
+++ app/src/main/java/com/example/agrilearninghub/ui/more/MoreTab.kt
@@ -0,0 +1,30 @@
+package com.example.agrilearninghub.ui.more
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.example.agrilearninghub.ui.core.screen.EmptyScreen
+
+object MoreTab : Tab {
+    private fun readResolve(): Any = MoreTab
+
+    override val options: TabOptions
+        @Composable get() {
+            return TabOptions(
+                index = 0u,
+                title = "Home",
+                icon =
+                    rememberVectorPainter(
+                        image = Icons.Default.MoreHoriz
+                    )
+            )
+        }
+
+    @Composable
+    override fun Content() {
+        EmptyScreen("Coming Screen")
+    }
+}
---
+++ app/src/main/java/com/example/agrilearninghub/ui/root/RootScreen.kt
@@ -0,0 +1,3 @@
+package com.example.agrilearninghub.ui.root
+
+class RootScreen
