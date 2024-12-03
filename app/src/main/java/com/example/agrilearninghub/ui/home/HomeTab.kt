60d37e4db4b171da22c0d955dbfcf62947f148a9
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2024-12-04 04:33:17 +0600

    feat: Update HomeTab UI with new layout, images, and welcome text in Bengali; set debug signing config

--- app/src/main/java/com/example/agrilearninghub/ui/home/HomeTab.kt
+++ app/src/main/java/com/example/agrilearninghub/ui/home/HomeTab.kt
@@ -1,12 +1,26 @@
 package com.example.agrilearninghub.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import com.example.agrilearninghub.ui.core.screen.EmptyScreen
+import com.example.agrilearninghub.R
 
 object HomeTab : Tab {
     private fun readResolve(): Any = HomeTab
@@ -25,6 +39,49 @@ object HomeTab : Tab {
 
     @Composable
     override fun Content() {
-        EmptyScreen("Coming Soon")
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.size(8.dp))
+            Image(
+                painter = painterResource(id = R.drawable.asset_3),
+                contentDescription = "Home_Image",
+                modifier =
+                    Modifier
+                        .size(220.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Column(
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.agri_learning_hub_logo),
+                    contentDescription = "Home_Image",
+                    modifier =
+                        Modifier
+                            .size(120.dp)
+                            .padding(start = 8.dp)
+                )
+                Text(text = "এগ্রি লার্নিং হাবে স্বাগতম!", fontSize = 24.sp)
+                Text(
+                    text = "এখানে আপনি ফসল, সবজি, উদ্ভিদ, চাষাবাদ সম্পর্কিত তথ্য এবং আরও অনেক কিছু শিখতে পারবেন!",
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center
+                )
+                Text(text = "চলুন শুরু করা যাক।", fontSize = 18.sp)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.asset_2),
+                contentDescription = "Home_Image",
+                modifier =
+                    Modifier
+                        .size(150.dp)
+                        .padding(end = 8.dp)
+                        .align(Alignment.End)
+            )
+        }
     }
 }
